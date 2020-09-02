99e8c7a9889efb24cce12336f175df1d1b413e18
liuxuefeng <996333715@qq.com>
2020-09-02 19:22:56 +0800

    dashijian

---
+++ assets/js/API.js
@@ -0,0 +1,9 @@
+// 发送请求之前执行
+// options:请求参数对象
+$.ajaxprefilter(function (options) {
+    // console.log(options);
+    // 在发起真正的Ajax请求之前,统一拼接请求的根路径
+    options.url = 'http://ajax.frontend,itheima.net' + options.url
+
+
+})
